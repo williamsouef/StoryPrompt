b6d618fea6370fb179ee7853b5e2e42d2aed0d91
WILLIAM SOUEF <williamsouef@gmail.com>
2022-09-05 20:32:09 +0200

    Initial Commit

---
+++ StoryPrompt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryPrompt
+//
+//  Created by William Souef on 05/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
